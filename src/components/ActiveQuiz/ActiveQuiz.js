48bf4f7ea16da742533a02852ab749ef3cda3bad
introkun <introkun@gmail.com>
2020-12-25 11:35:16 +0300

    lesson 49 done

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './ActiveQuiz.module.css'
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>2.</strong>&nbsp;
+                Как дела?
+            </span>
+
+            <small>4 из 12</small>
+        </p>
+
+
+    </div>
+)
+
+export default ActiveQuiz
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import classes from './Layout.module.css'
-
 class Layout extends Component {
     render() {
         return (
